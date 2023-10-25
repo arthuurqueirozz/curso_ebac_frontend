40103e6d1f8e81a26e1a3664eee13483927c3219
Arthur Queiroz <arthurqueiroz25@gmail.com>
2023-10-24 21:19:25 -0300

    update script.js, style.css

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-
 const form = document.getElementById('form-check')
 const message = document.getElementById('message')
+const numberOne = document.getElementById('number-1')
+const numberTwo = document.getElementById('number-2')
+
 
 form.addEventListener('submit', (e) => {
-    const numberOne = document.getElementById('number-1')
-    const numberTwo = document.getElementById('number-2')
-    
-     if(numberOne.value > numberTwo.value) {
-        console.log(numberOne.value)
-     }
+      e.preventDefault()
+
+     if(numberOne.value < numberTwo.value) {
+        message.style.display = 'block'
+        message.innerHTML = 'Número B maior que número A'
+        message.classList.add('right')
+     } 
+      if(numberOne.value >= numberTwo.value){
+         message.style.display = 'block'
+         message.innerHTML = 'Número B não é maior que número A'
+         message.classList.add('wrong')
+      }
 })
