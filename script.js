ed66315b7fee1af8da140975d1e137699270823a
Arthur Queiroz <arthurqueiroz25@gmail.com>
2023-10-24 21:35:34 -0300

    update script.js

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ const numberTwo = document.getElementById('number-2')
 form.addEventListener('submit', (e) => {
       e.preventDefault()
 
-     if(numberOne.value < numberTwo.value) {
+      if(Number(numberOne.value) < Number(numberTwo.value)) {
         message.style.display = 'block'
         message.innerHTML = 'Número B maior que número A'
+        message.classList.remove('wrong')
         message.classList.add('right')
      } 
-      if(numberOne.value >= numberTwo.value){
+      if(Number(numberOne.value) >= Number(numberTwo.value)){
          message.style.display = 'block'
          message.innerHTML = 'Número B não é maior que número A'
+         message.classList.remove('right')
          message.classList.add('wrong')
       }
 })
