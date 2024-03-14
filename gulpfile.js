4ccf8bdd2bf30ddf88785516c97b454ca5ed2325
Arthur Queiroz <arthurqueiroz25@gmail.com>
2024-03-14 19:50:16 -0300

    adding image compressor

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,14 @@ const gulp = require('gulp')
 const sourcemaps = require('gulp-sourcemaps')
 const uglify = require('gulp-uglify')
 const obfuscate = require('gulp-obfuscate')
+const imagemin = require('gulp-imagemin')
 
-
+function imageCompressor() {
+    return gulp.src('./src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
+}
+ 
 function compileSass() {
     return gulp.src('./src/styles/main.scss')
         .pipe(sourcemaps.init())
@@ -25,4 +31,5 @@ function jsCompressor() {
 exports.default = () => {
     gulp.watch('./src/styles/*.scss', {ignoreInitia: false}, gulp.series(compileSass))
     gulp.watch('./src/scripts/*js', {ignoreInitia: false}, gulp.series(jsCompressor))
+    gulp.watch('./src/images/*', {ignoreInitia: false}, gulp.series(imageCompressor))
 }
